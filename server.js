dd6f3fb5edb2628939fec87a23223e2f20756c95
kanhery <kanhery>
2017-06-23 14:26:14 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', hbs);
+app.use(express.static(__dirname+'/public'));
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log= `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) { console.log('Unable to append to server log');
+        }
+    });
+    console.log(log);
+
+    next();
+});
+app.use((req,res,next) => {
+    res.render('maintenance.hbs')
+});
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+})
+app.get('/', (req,res) =>{
+    res.render('home.hbs',{
+        pagetitle:'Home Page',
+        welomeNote: 'Welcome to my Website'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pagetitle:'AboutUs Page'
+    });
+});
+
+app.listen(8000,()=>{
+    console.log("Server started listening on port 8000...");
+});
